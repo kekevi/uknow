9215e07605064cb29bf774251a613ff095a8f5e7
kekevi <54363826+kekevi@users.noreply.github.com>
2021-05-08 14:54:02 -0400

    Update constants.js

--- final_project/src/constants.js
+++ final_project/src/constants.js
@@ -1,3 +1,7 @@
 export const DOMAIN_NAME = "http://localhost:3001";
 export const HTTP_TIMEOUT = 3000;
 export const CORS_SITE = "http://localhost";
+
+/*
+  the other possible DOMAIN_NAME is "http://kekevi.pythonanywhere.com";
+*/
